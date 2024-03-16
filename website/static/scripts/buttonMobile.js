2e482bd705448bbef547d37ed2710a1b476d335d
Juliano <juliano.co.lopes@gmail.com>
2024-03-16 12:36:14 -0300

    made menu appears functionality

--- website/static/scripts/buttonMobile.js
+++ website/static/scripts/buttonMobile.js
@@ -1,9 +1,16 @@
 const btn = document.querySelector(".button-menu-mobile");
-const menuNavigation = document.querySelector("#menu-nav");
+const menuNavigation = document.querySelector(".menu-navigation");
 
-btn.addEventListener("click", function() {
-    //e.preventDefault();
+btn.onclick = function() {
     btn.classList.toggle('active');
-    
-    menuNavigation.classList.toggle('menu-visible');
-});
+    menuNavigation.classList.contains("menu-visible") ? menuNavigation.classList.remove("menu-visible") : menuNavigation.classList.add("menu-visible");
+    if (!menuNavigation.classList.contains("menu-visible")) {
+        menuNavigation.style.top = "0";
+        menuNavigation.style.transform = "translateY(calc(-250px - 4em - 15px))";
+        menuNavigation.style.transition = "0.3s ease-out";
+    } else {
+        menuNavigation.style.top = "-250px";
+        menuNavigation.style.transform = "";
+        menuNavigation.style.transition = "";
+    }
+};
