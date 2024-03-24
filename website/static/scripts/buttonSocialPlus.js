af8e584509823196e394b266083ce02e6a54b9f8
Juliano <juliano.co.lopes@gmail.com>
2024-03-24 12:14:14 -0300

    added form edit fields form to edit

---
+++ website/static/scripts/buttonSocialPlus.js
@@ -0,0 +1,22 @@
+const socialPlus = document.querySelector(".plus-button-field");
+var newSocialFields = document.querySelectorAll(".plus-field");
+console.log(newSocialFields)
+
+socialPlus.addEventListener('click', function() {
+    for (let i = 0; i < 1; i++) {
+        newSocialFields[i].style.display = 'flex';
+        newSocialFields[i].classList.remove('plus-field');
+        newSocialFields[i + 1].style.display = 'flex';
+        newSocialFields[i + 1].classList.remove('plus-field');
+        newSocialFields = document.querySelectorAll(".plus-field");
+    }
+    checkMaxNodes();
+    console.log(newSocialFields)
+});
+
+function checkMaxNodes() {
+    if (newSocialFields.length == 0) {
+        socialPlus.style.display = 'none';
+        document.querySelector(".save-button").style.marginTop = "3vh";
+    }
+}
