0fcadf01d114e084d8a14e8ca061c6384e1e9ef4
Juliano <juliano.co.lopes@gmail.com>
2024-03-31 00:49:00 -0300

    started the team page and some improvements in menu

--- website/views/AuthorView.py
+++ website/views/AuthorView.py
@@ -31,29 +31,23 @@ def edit_author_profile(request, slug):
         social_forms.append(SocialMediaForm(instance=social))
     social_empty_form = SocialMediaForm()
     send_message = False
-    
+
     if request.POST:
-        print(request.POST)
         social_forms = []
         for social in author_social_media:
             social_forms.append(SocialMediaForm(request.POST, instance=social))
         author_request_post = check_request_post(request)
-
         if author_request_post is not None:
             if author.image != '' and author_request_post['image'] is not None:
                 author.image.delete(save=True)
                 author.image = author_request_post['image']
-        
         if check_author_form(request, author):
             if update_social_media(request, author_social_media):
-                print("REACHING UPDATE: ", "\n")
                 send_message = True
             if author_request_post['new_social_addition'] and not any(author_request_post['exclude_social_media']):
-                print("REACHING CREATE: ", author_request_post['new_social_addition'], "\n")
                 create_social_media(request, author_request_post)
                 send_message = True
             if any(author_request_post['exclude_social_media']):
-                print("REACHING EXCLUDE AND CHECK ANY: ", any(author_request_post['exclude_social_media']), "\n", author_request_post['exclude_social_media'], "\n")
                 exclude_social_media(request, author)
                 send_message = True
             if send_message:
@@ -72,8 +66,6 @@ def edit_author_profile(request, slug):
 def check_request_post(request):
     author_post_request_data = None
     if request.POST:
-        print("CHECK REQUEST FUNCTION | LEN AUTHOR: ", len(Author.objects.get(user__username=request.POST['username']).social_media.all()), "\n")
-        print("CHECK REQUEST FUNCTION | LEN REQUEST SOCIAL MEDIA PROFILE", len(request.POST.getlist('social_media_profile')), "\n")
         author_post_request_data = {
             "username": request.POST['username'],
             "name": request.POST['author_name'],
@@ -82,15 +74,13 @@ def check_request_post(request):
             "new_social_addition": len(request.POST.getlist('social_media_profile')) > len(Author.objects.get(user__username=request.POST['username']).social_media.all()),
             "exclude_social_media": request.POST.getlist('exclude-social'),
         }
-    print("PRINTING AUTHOR POST REQUEST: ", author_post_request_data, "\n")
+
     return author_post_request_data
 
 def check_user_form(request, author):
     user_form = UserForm(request.POST, instance=request.user)
     author_user = check_request_post(request)
     username_free = author_user['check_username_request'] != author.user.username
-    print("Author USER REQUEST: ", author_user['check_username_request'], "\n")
-    print("SAVED AUTHOR USERNAME: ", author.user.username, "\n")
     if user_form.is_valid() and author_user['username'] != author.user.username and username_free:
         user_author = get_object_or_404(User, id=author.user.id)
         user_author.username = author_user['username']
@@ -108,7 +98,6 @@ def check_author_form(request, author):
 
 def update_social_media(request, author_social_media):
     social_media_request_post = list(zip(request.POST.getlist('social_media'), request.POST.getlist('social_media_profile')))
-    print("UPDATE: ", social_media_request_post)
     updated = False
     for i in range(len(author_social_media)):
         if (author_social_media[i].social_media != int(social_media_request_post[i][0])) or (author_social_media[i].social_media_profile != social_media_request_post[i][1]):
@@ -116,12 +105,9 @@ def update_social_media(request, author_social_media):
             author_social_media[i].social_media_profile = social_media_request_post[i][1]
             author_social_media[i].save()
             updated = True
-    print(updated)
     return updated
-
 def create_social_media(request, author_request_post):
     social_media_request_post = list(zip(request.POST.getlist('social_media'), request.POST.getlist('social_media_profile')))
-    print("CREATE: ", social_media_request_post, "\n")
     author = Author.objects.get(user__username=author_request_post['username'])
     social = author.social_media.all()
 
@@ -133,7 +119,7 @@ def create_social_media(request, author_request_post):
         )
         new_social.save()
         author.social_media.add(new_social)
-            
+
 def exclude_social_media(request, author):
     exclusions = check_request_post(request)['exclude_social_media']
     for exclude_request in exclusions:
---
+++ website/views/TeamView.py
@@ -0,0 +1,12 @@
+from website.models.AuthorModel import Author
+from website.models.__init__ import ACADEMIC_LEVEL, SOCIAL_MEDIA
+from django.shortcuts import render, redirect, get_object_or_404
+
+def view_team(request):
+    team = Author.objects.all()
+    context = {
+        'authors': team,
+        'social_media_index': SOCIAL_MEDIA,
+        'graduations_level': ACADEMIC_LEVEL,
+    }
+    return render(request, template_name="our-team/our-team.html", context=context)
