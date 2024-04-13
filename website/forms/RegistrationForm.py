f84db4a51e86f596b68ec6ea70b8b250af6f8651
Juliano <juliano.co.lopes@gmail.com>
2024-04-13 06:14:12 -0300

    made: register page

--- website/forms/RegistrationForm.py
+++ website/forms/RegistrationForm.py
@@ -1,32 +1,39 @@
 from website.forms import *
-from website.models import Author
-from website.models import User
+from website.models import Author, Reader, User
 from django.contrib.auth.forms import UserCreationForm as BaseUserCreationForm
 
 
-class RegistrationForm(forms.ModelForm):
-
-    password1 = forms.CharField(label='Password', widget=forms.PasswordInput, required=True)
-    password2 = forms.CharField(label='Confirm Password', widget=forms.PasswordInput, required=True)
+class RegistrationAuthorForm(forms.ModelForm):
 
     author_name = forms.TextInput()
     
-
     class Meta:
         model = Author
         fields = ('author_name',)
-    
 
-    def clean_password2(self):
-        password1 = self.cleaned_data.get('password1')
-        password2 = self.cleaned_data.get('password2')
-        if password1 and password2 and password1 != password2:
-            raise forms.ValidationError('Passwords do not match.')
-        return password2
+
+class RegistrationReaderForm(forms.ModelForm):
+
+    reader_name = forms.TextInput()
+    
+    class Meta:
+        model = Reader
+        fields = ('reader_name',)
+
 
 
 class UserCreationForm(BaseUserCreationForm):
 
     class Meta:
         model = User
-        fields = ("username", "email")
+        fields = ("email",)
+        
+    password1 = forms.CharField(label='Senha:', widget=forms.PasswordInput, required=True)
+    password2 = forms.CharField(label='Confirme a senha:', widget=forms.PasswordInput, required=True)
+    
+    """def clean_password2(self):
+        password1 = self.cleaned_data.get('password1')
+        password2 = self.cleaned_data.get('password2')
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError('Passwords do not match.')
+        return password2"""
--- website/views/SignUpView.py
+++ website/views/SignUpView.py
@@ -1,14 +1,65 @@
 from django.contrib.auth import logout
 from django.shortcuts import render, redirect
 from django.urls import reverse
-from django.contrib.auth.hashers import check_password
-from website.models import User
-from website.models import Author
+from website.models import User, Author, Reader
 from django.contrib import messages
 from django.contrib.messages import get_messages
 
-def sign_up(request):
-    
-    context = {}
-    #context.add("user", )
+def sign_up_user(request):
+    print(request.POST)
+    if request.POST:
+        cont = len(User.objects.all()) + 1
+        
+        treated_username = request.POST.get("nome", None).split(" ")[0].lower() + f"-user{cont}"
+        
+        password1 = request.POST.get("password1", None)
+        password2 = request.POST.get("password2", None)
+        
+        if password1 == password2:
+            user = User(
+                username = treated_username,
+                email = request.POST.get("email", None),
+                password = request.POST.get('password2', None),
+            )
+            user.save()
+            
+            type_user_created = create_author(request, user) if request.POST.get("tipo-user") == "author" else create_reader(request, user)
+            messages.success(request, f"O usuário {user.username} foi registrado com sucesso.")
+            print("\nDefinido messages \n")
+            return redirect("login")
+        else:
+            #print(messages.get_messages(request))
+            messages.error(request, "A senha digitada não confere.")
+            
+
     return render(request, template_name="sign-up/sign-up.html", status=200)
+
+
+def create_author(request, user):
+    author_name = request.POST.get("nome", None)
+    slug = str()
+    for n in author_name.split(" "):
+        if n == author_name.split(" ")[-1]:
+            slug += n.lower()
+        else:
+            slug += f"{n.lower()}-"
+    
+    author = Author(
+       user = user,
+       author_name = author_name,
+       author_url_slug = slug,
+       access_level = 1,
+    )
+    author.save()
+    return author
+
+
+def create_reader(request, user):
+    reader_name = request.POST.get("nome", None)
+    reader = Reader(
+        user = user,
+        reader_name = reader_name,
+        access_level = 2,
+    )
+    reader.save()
+    return reader
