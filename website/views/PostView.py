7e83f572140d64e2edd4669aa7afd0809538ff27
Juliano <juliano.co.lopes@gmail.com>
2025-08-31 19:52:48 -0300

    style: Post Page with author card

--- website/views/PostView.py
+++ website/views/PostView.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.decorators.clickjacking import xframe_options_exempt
+from website.models import ACADEMIC_LEVEL, SOCIAL_MEDIA
 from website.models.PostModel import Post
 
 
@@ -7,4 +8,8 @@ from website.models.PostModel import Post
 def post_detail(request, url_slug):
     # Get the post by its URL slug or return 404 if not found
     post = get_object_or_404(Post, url_slug=url_slug)
-    return render(request, 'post/post_detail.html', {'post': post})
+    context = {'post': post}
+    context['author_connected'] = request.user == post.author.user
+    context['graduations_level'] = ACADEMIC_LEVEL
+    context['social_media_index'] = SOCIAL_MEDIA
+    return render(request, 'post/post_detail.html', context)
