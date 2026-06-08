e44e9283a2b2c06a828cff94cff75e3a26995892
Juliano <juliano.co.lopes@gmail.com>
2026-06-08 16:09:01 -0300

    refactor(author): refatoração páginas de exibição do autor e da equipe.
    Refatorado author.html com história, formações e experiências; alinhado our-team
    com card-author responsivo e estendido author-page.css nos três breakpoints.

--- website/views/author/TeamView.py
+++ website/views/author/TeamView.py
@@ -1,14 +1,11 @@
 from django.shortcuts import render
 
-from website.models import ACADEMIC_LEVEL, SOCIAL_MEDIA
 from website.models.author.AuthorModel import Author
 
 
 def view_team(request):
-    team = Author.objects.all()
+    authors = Author.objects.prefetch_related("graduations", "jobs", "social_media").all()
     context = {
-        "authors": team,
-        "social_media_index": SOCIAL_MEDIA,
-        "graduations_level": ACADEMIC_LEVEL,
+        "authors": authors,
     }
     return render(request, template_name="blog/pages/our-team/our-team.html", context=context)
